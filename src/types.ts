6ddbee40b85e8c1b330603005a2ae1260c0a2b09
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2026-06-11 10:36:47 +0530

    feat: add types entry point export

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export type { ValidDataBrand } from "./types/brand.helper.js";
+export type {
+  GqlRequestConfig,
+  SchemaConfig,
+  GqlAsyncResolver,
+  VerifiedArgs,
+  GqlGuardMiddleware,
+  GqlContextAndArgsMiddleware,
+} from "./types/graphql/resolver.types.js";
+export type {
+  GqlGlobalBaseContext,
+  MagicInjectedContext,
+  VerifiedContext,
+} from "./types/graphql/context.types.js";
