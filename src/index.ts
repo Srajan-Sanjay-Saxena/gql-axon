0fa3557cb60a644f24dbe5c1399976571c5ad1be
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2026-06-11 10:36:46 +0530

    feat: add main entry point export

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { MiddlewareChainPipeline, withChainedMiddleware } from "./core/chain.middleware.js";
+export { RequestGuardMiddleware, MakeObjectTypeSafeEngine } from "./core/engine.js";
+export type { TypeSafeObject } from "./core/engine.js";
